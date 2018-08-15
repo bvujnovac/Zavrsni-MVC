c2eb3a87a35e6603f2c772a17875eb83fabd1b48
bvujnovac <bvujnovac@gmail.com>
2018-08-15 11:50:03 +0200

    implementing per profile table insert model

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,59 +3,58 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-    public function __construct()
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('get_data');
+    $this->load->helper('url');
+  }
+  public function index()
+  {
+    //getting data from the model
+    $data['sensors'] = $this->get_data->load_data();
+    $values['min'] = $data['sensors']['min'];
+    $values['max'] = $data['sensors']['max'];
+
+    //rendering views
+    $this->load->view('header_view');
+    $this->load->view('home_view', $values);
+    $this->load->view('footer_view');
+  }
+  public function data()
+  {
+    $startDate = $this->input->get('time_from', TRUE); //getting date for SQL query, and sanitizing it.
+    $endDate = $this->input->get('time_to', TRUE); //getting date for SQL query, and sanitizing it.
+
+    if ($startDate && $endDate)
     {
-        parent::__construct();
-        $this->load->model('get_data');
-        $this->load->helper('url');
+
+      $data['values'] = $this->get_data->load_ajax_data($startDate, $endDate);
+      $final_data['temperature'] = $data["values"]["temperature"];
+      $final_data['light'] = $data["values"]["light"];
+      $final_data['moist'] = $data["values"]["moist"];
+      $final_data['phvalue'] = $data["values"]["phvalue"];
+
+      $this->load->view('data_view', $final_data);
     }
-    public function index()
+    else
     {
-        //getting data from the model
-        $data['sensors'] = $this->get_data->load_data();
-        $values['min'] = $data['sensors']['min'];
-        $values['max'] = $data['sensors']['max'];
-
-        //rendering views
-        $this->load->view('header_view');
-        $this->load->view('home_view', $values);
-        $this->load->view('footer_view');
+      header("HTTP/1.1 406 Not Acceptable");
     }
-    public function data()
-    {
-        $startDate = $this->input->get('time_from', TRUE); //getting date for SQL query, and sanitizing it.
-        $endDate = $this->input->get('time_to', TRUE); //getting date for SQL query, and sanitizing it.
+  }
+  public function add()
+  {
+    $id = $this->input->get('id', TRUE); //getting temperature for SQL query, and sanitizing it.
+    $temperature = $this->input->get('temperature', TRUE); //getting temperature for SQL query, and sanitizing it.
+    $light = $this->input->get('light', TRUE); //getting light for SQL query, and sanitizing it.
+    $moist = $this->input->get('moist', TRUE); //getting moist for SQL query, and sanitizing it.
+    $phvalue = $this->input->get('phvalue', TRUE); //getting phvalue for SQL query, and sanitizing it.
 
-        if ($startDate && $endDate)
-        {
-
-            $data['values'] = $this->get_data->load_ajax_data($startDate, $endDate);
-            $final_data['temperature'] = $data["values"]["temperature"];
-            $final_data['light'] = $data["values"]["light"];
-            $final_data['moist'] = $data["values"]["moist"];
-            $final_data['phvalue'] = $data["values"]["phvalue"];
-
-            $this->load->view('data_view', $final_data);
-        }
-        else
-        {
-            header("HTTP/1.1 406 Not Acceptable");
-        }
+    if ($id && $temperature && $light && $moist && $phvalue) {
+      $this->get_data->add_data($id, $temperature, $light, $moist, $phvalue);
     }
-    public function add()
-    {
-      $temperature = $this->input->get('temperature', TRUE); //getting temperature for SQL query, and sanitizing it.
-      $light = $this->input->get('light', TRUE); //getting light for SQL query, and sanitizing it.
-      $moist = $this->input->get('moist', TRUE); //getting moist for SQL query, and sanitizing it.
-      $phvalue = $this->input->get('phvalue', TRUE); //getting phvalue for SQL query, and sanitizing it.
-
-      if ($temperature && $light && $moist && $phvalue) {
-        $addData = array('temperature' => $temperature,'light' => $light,'moist' => $moist,'phvalue' => $phvalue);
-
-        $this->db->insert('datalog', $addData);
-      }
-      else {
-        header("HTTP/1.1 406 Not Acceptable");
-      }
+    else {
+      header("HTTP/1.1 406 Not Acceptable");
     }
+  }
 }
--- application/models/Get_data.php
+++ application/models/Get_data.php
@@ -1,107 +1,120 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: beni
- * Date: 04.04.18.
- * Time: 18:28
- */
+* Created by PhpStorm.
+* User: beni
+* Date: 04.04.18.
+* Time: 18:28
+*/
 class Get_data extends CI_Model {
 
-    public function __construct()
+  public function __construct()
+  {
+    $this->load->database();
+  }
+
+  //method to retrieve data from database and format it.
+  public function load_data()
+  {
+    $sql1 = "SELECT DATE_FORMAT(`timeStamp`, '%Y-%m-%d') AS `timeMin` FROM `datalog` WHERE timeStamp=(select min(timeStamp) from datalog)";
+    $sql2 = "SELECT DATE_FORMAT(`timeStamp`, '%Y-%m-%d') AS `timeMax` FROM `datalog` WHERE timeStamp=(select max(timeStamp) from datalog)";
+
+    $minmaxArray = array();
+
+    $query1 = $this->db->query($sql1);
+    $query2 = $this->db->query($sql2);
+
+    foreach ($query1->result() as $row)
     {
-        $this->load->database();
-    }
-
-    //method to retrieve data from database and format it.
-    public function load_data()
-    {
-        $sql1 = "SELECT DATE_FORMAT(`timeStamp`, '%Y-%m-%d') AS `timeMin` FROM `datalog` WHERE timeStamp=(select min(timeStamp) from datalog)";
-        $sql2 = "SELECT DATE_FORMAT(`timeStamp`, '%Y-%m-%d') AS `timeMax` FROM `datalog` WHERE timeStamp=(select max(timeStamp) from datalog)";
-
-        $minmaxArray = array();
-
-        $query1 = $this->db->query($sql1);
-        $query2 = $this->db->query($sql2);
-
-        foreach ($query1->result() as $row)
-        {
-            $min = $row->timeMin;
-
-        }
-
-        foreach ($query2->result() as $row)
-        {
-            $max = $row->timeMax;
-
-        }
-
-        function array_push_assoc($array, $key, $value){
-            $array[$key] = $value;
-            return $array;
-        }
-
-        $minmaxArray = array_push_assoc($minmaxArray,'min', $min);
-        $minmaxArray = array_push_assoc($minmaxArray,'max', $max);
-
-        return $minmaxArray;
-    }
-
-
-    public function load_ajax_data($start, $end)
-    {
-
-        $startDate = $start; //fixed data for now
-        $endDate = $end; //fixed date for now.
-
-        $sql = "SELECT `timeStamp` AS `time`, `temperature`, `light`, `moist`, `phvalue` FROM `datalog` WHERE `timeStamp` BETWEEN '{$startDate}' AND '{$endDate}' ORDER BY `timeStamp` ASC";
-
-        //$query= $this->db->get('datalog');
-
-        $jsonArrayTemperature = array();
-        $jsonArrayLight = array();
-        $jsonArrayMoist = array();
-        $jsonArrayPhvalue = array();
-        $finalArray = array();
-
-        $query = $this->db->query($sql);
-
-        foreach ($query->result() as $row)
-        {
-            $jsonArrayItemTemperature = array();
-            $jsonArrayItemLight = array();
-            $jsonArrayItemMoist = array();
-            $jsonArrayItemPhvalue = array();
-            $jsonArrayItemTemperature['label'] = $row->time;
-            $jsonArrayItemTemperature['value'] = $row->temperature;
-            $jsonArrayItemLight['label'] = $row->time;
-            $jsonArrayItemLight['value'] = $row->light;
-            $jsonArrayItemMoist['label'] = $row->time;
-            $jsonArrayItemMoist['value'] = $row->moist;
-            $jsonArrayItemPhvalue['label'] = $row->time;
-            $jsonArrayItemPhvalue['value'] = $row->phvalue;
-            //append the above created arrays into the main array.
-            array_push($jsonArrayTemperature, $jsonArrayItemTemperature);
-            array_push($jsonArrayLight, $jsonArrayItemLight);
-            array_push($jsonArrayMoist, $jsonArrayItemMoist);
-            array_push($jsonArrayPhvalue, $jsonArrayItemPhvalue);
-        }
-        //function to create
-        function array_push_assoc($array, $key, $value){
-            $array[$key] = $value;
-            return $array;
-        }
-
-        $finalArray = array_push_assoc($finalArray,'temperature', $jsonArrayTemperature);
-        $finalArray = array_push_assoc($finalArray,'light', $jsonArrayLight);
-        $finalArray = array_push_assoc($finalArray,'moist', $jsonArrayMoist);
-        $finalArray = array_push_assoc($finalArray,'phvalue', $jsonArrayPhvalue);
-
-
-        return $finalArray;
+      $min = $row->timeMin;
 
     }
 
-    public function add_data()
+    foreach ($query2->result() as $row)
     {
+      $max = $row->timeMax;
+
     }
+
+    function array_push_assoc($array, $key, $value){
+      $array[$key] = $value;
+      return $array;
+    }
+
+    $minmaxArray = array_push_assoc($minmaxArray,'min', $min);
+    $minmaxArray = array_push_assoc($minmaxArray,'max', $max);
+
+    return $minmaxArray;
+  }
+
+
+  public function load_ajax_data($start, $end)
+  {
+
+    $startDate = $start; //fixed data for now
+    $endDate = $end; //fixed date for now.
+
+    $sql = "SELECT `timeStamp` AS `time`, `temperature`, `light`, `moist`, `phvalue` FROM `datalog` WHERE `timeStamp` BETWEEN '{$startDate}' AND '{$endDate}' ORDER BY `timeStamp` ASC";
+
+    //$query= $this->db->get('datalog');
+
+    $jsonArrayTemperature = array();
+    $jsonArrayLight = array();
+    $jsonArrayMoist = array();
+    $jsonArrayPhvalue = array();
+    $finalArray = array();
+
+    $query = $this->db->query($sql);
+
+    foreach ($query->result() as $row)
+    {
+      $jsonArrayItemTemperature = array();
+      $jsonArrayItemLight = array();
+      $jsonArrayItemMoist = array();
+      $jsonArrayItemPhvalue = array();
+      $jsonArrayItemTemperature['label'] = $row->time;
+      $jsonArrayItemTemperature['value'] = $row->temperature;
+      $jsonArrayItemLight['label'] = $row->time;
+      $jsonArrayItemLight['value'] = $row->light;
+      $jsonArrayItemMoist['label'] = $row->time;
+      $jsonArrayItemMoist['value'] = $row->moist;
+      $jsonArrayItemPhvalue['label'] = $row->time;
+      $jsonArrayItemPhvalue['value'] = $row->phvalue;
+      //append the above created arrays into the main array.
+      array_push($jsonArrayTemperature, $jsonArrayItemTemperature);
+      array_push($jsonArrayLight, $jsonArrayItemLight);
+      array_push($jsonArrayMoist, $jsonArrayItemMoist);
+      array_push($jsonArrayPhvalue, $jsonArrayItemPhvalue);
+    }
+    //function to create
+    function array_push_assoc($array, $key, $value){
+      $array[$key] = $value;
+      return $array;
+    }
+
+    $finalArray = array_push_assoc($finalArray,'temperature', $jsonArrayTemperature);
+    $finalArray = array_push_assoc($finalArray,'light', $jsonArrayLight);
+    $finalArray = array_push_assoc($finalArray,'moist', $jsonArrayMoist);
+    $finalArray = array_push_assoc($finalArray,'phvalue', $jsonArrayPhvalue);
+
+    return $finalArray;
+  }
+
+  public function add_data($id, $temperature, $light, $moist, $phvalue)
+  {
+    $id = $id
+    $temperature = $temperature
+    $light = $light
+    $moist = $moist
+    $phvalue = $phvalue
+
+    if ($this->db->table_exists($id))
+    {
+      $addData = array('temperature' => $temperature,'light' => $light,'moist' => $moist,'phvalue' => $phvalue);
+      $this->db->insert($id, $addData);
+    }
+    else {
+      // table does not exist
+    }
+
+  }
 }
